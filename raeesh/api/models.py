c9f5439c94a7414d34fa7e6be154b2b55e312a3c
himumbai <102490231+himumbai@users.noreply.github.com>
2022-03-28 04:13:29 +0530

    Update models.py

--- raeesh/api/models.py
+++ raeesh/api/models.py
@@ -7,5 +7,6 @@ from django.db import models
 class Student(models.Model):
     id=models.IntegerField(primary_key=True,max_length=20)
     name=models.CharField(max_length=30)
+    age=models.IntegerField(max_length=20)
     city=models.CharField(max_length=20)
     myimage=models.ImageField(default=0,upload_to='images/')
